140359ed5d2ad7889ec62f77ed87014a94710a2e
MacPro <762293490@qq.com>
2019-03-24 11:42:01 +0800

    crawl with proxy

--- crawler.py
+++ crawler.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-# coding=utf-8
 
 import os
-import urllib
 import json
 import re
 
-import multiprocessing
+# import urllib
+# import multiprocessing
 import requests
 from lxml import etree
 import fire
@@ -14,14 +13,22 @@ from loguru import logger
 logger.add("logs/%s.log" % __file__.rstrip('.py'), format="{time:MM-DD HH:mm:ss} {level} {message}")
 
 headers = {
-    'User-Agent':
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
 }
+proxies = {}
+
+# 如果代理不稳定，不推荐使用
+# local showdowsocks service
+# proxies example:
+# proxies = {
+#     "http": "socks5://127.0.0.1:1080",
+#     "https": "socks5://127.0.0.1:1080",
+# }
 
 
 def list_page(url):
     logger.info('crawling : %s' % url)
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=headers, proxies=proxies)
     html = etree.HTML(resp.text)
     vkeys = html.xpath('//*[@class="phimage"]/div/a/@href')
     gif_keys = html.xpath('//*[@class="phimage"]/div/a/img/@data-mediabook')
@@ -32,27 +39,28 @@ def list_page(url):
         item['gif_url'] = gif_keys[i]
         try:
             if 'ph' in item['vkey']:
-                # jobs.append(gevent.spawn(download, item['gif_url'], item['vkey'],'webm'))
-                p = multiprocessing.Process(target=download, args=(item['gif_url'], item['vkey'], 'webm'))
-                p.start()
+                download(item['gif_url'], item['vkey'], 'webm')
+                # p = multiprocessing.Process(target=download, args=(item['gif_url'], item['vkey'], 'webm'))
+                # p.start()
         except Exception as err:
             logger.error(err)
-    # gevent.joinall(jobs, timeout=2)
 
 
 def detail_page(url):
     s = requests.Session()
-    resp = s.get(url, headers=headers)
+    resp = s.get(url, headers=headers, proxies=proxies)
     html = etree.HTML(resp.content)
     title = ''.join(html.xpath('//h1//text()')).strip()
+    logger.info(title)
 
     js = html.xpath('//*[@id="player"]/script/text()')[0]
-    tem = re.findall('var\\s+\\w+\\s+=\\s+(.*);\\s+var player_mp4_seek',js)[-1]
+    tem = re.findall('var\\s+\\w+\\s+=\\s+(.*);\\s+var player_mp4_seek', js)[-1]
     con = json.loads(tem)
 
     for _dict in con['mediaDefinitions']:
         if 'quality' in _dict.keys() and _dict.get('videoUrl'):
             logger.info('%s %s' % (_dict.get('quality'), _dict.get('videoUrl')))
+            logger.info('start download...')
             try:
                 download(_dict.get('videoUrl'), title, 'mp4')
                 break  # 如下载了较高分辨率的视频 就跳出循环
@@ -65,8 +73,12 @@ def download(url, name, filetype):
     if os.path.exists(filepath):
         logger.info('this file had been downloaded :: %s' % filepath)
         return
-    urllib.request.urlretrieve(url, '%s' % filepath)
-    logger.info('download success :: %s' % filepath)
+    else:
+        rep = requests.get(url, headers=headers, proxies=proxies)
+        with open(filepath, 'wb') as file:
+            file.write(rep.content)
+        # urllib.request.urlretrieve(url, '%s' % filepath)
+        logger.info('download success :: %s' % filepath)
 
 
 def run(_arg=None):
@@ -76,17 +88,12 @@ def run(_arg=None):
             os.mkdir(path)
     if _arg == 'webm':
         # https://www.pornhub.com/categories
-        urls = [
-            'https://www.pornhub.com/video?o=tr',
-            'https://www.pornhub.com/video?o=ht',
-            'https://www.pornhub.com/video?o=mv',
-            'https://www.pornhub.com/video'
-        ]
-        # jobs = [gevent.spawn(list_page, url) for url in urls]
-        # gevent.joinall(jobs)
+        urls = ['https://www.pornhub.com/video?o=tr', 'https://www.pornhub.com/video?o=ht',
+                'https://www.pornhub.com/video?o=mv', 'https://www.pornhub.com/video']
         for url in urls:
-            p = multiprocessing.Process(target=list_page, args=(url, ))
-            p.start()
+            list_page(url)
+            # p = multiprocessing.Process(target=list_page, args=(url, ))
+            # p.start()
     elif _arg == 'mp4':
         with open('download.txt', 'r') as file:
             keys = list(set(file.readlines()))
@@ -96,10 +103,9 @@ def run(_arg=None):
                 continue
             url = 'https://www.pornhub.com/view_video.php?viewkey=%s' % key.strip()
             logger.info('url: {}', url)
-            # jobs.append(gevent.spawn(detail_page, url))
-            p = multiprocessing.Process(target=detail_page, args=(url, ))
-            p.start()
-        # gevent.joinall(jobs, timeout=2)
+            detail_page(url)
+            # p = multiprocessing.Process(target=detail_page, args=(url, ))
+            # p.start()
     else:
         _str = """
 tips:
@@ -111,8 +117,7 @@ tips:
         """
         logger.info(_str)
         return
-    logger.info('Using multiprocessing, commands send to system, please wait a moment.')
-    logger.info('Maybe there is some error, leave it alone.')
+    logger.info('finish !')
 
 
 if __name__ == '__main__':
