5000358ef66dc16e752942e9c1ea3273936ef69a
MacPro <ycm76229@gmail.com>
2019-12-10 10:06:31 +0800

    update crawler

--- crawler.py
+++ crawler.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
 import os
-import json
 import re
 
-# import urllib
-# import multiprocessing
+import js2py
 import requests
 from lxml import etree
 import fire
 from loguru import logger
-logger.add("logs/%s.log" % __file__.rstrip('.py'), format="{time:MM-DD HH:mm:ss} {level} {message}")
+logger.add("logs/%s.log" % __file__.rstrip('.py'),
+           format="{time:MM-DD HH:mm:ss} {level} {message}")
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+    'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
 }
 proxies = {}
 
@@ -28,11 +28,10 @@ proxies = {}
 
 def list_page(url):
     logger.info('crawling : %s' % url)
-    resp = requests.get(url, headers=headers, proxies=proxies)
+    resp = requests.get(url, headers=headers, proxies=proxies, verify=False)
     html = etree.HTML(resp.text)
     vkeys = html.xpath('//*[@class="phimage"]/div/a/@href')
     gif_keys = html.xpath('//*[@class="phimage"]/div/a/img/@data-mediabook')
-    # jobs = []
     for i in range(len(vkeys)):
         item = {}
         item['vkey'] = vkeys[i].split('=')[-1]
@@ -40,32 +39,39 @@ def list_page(url):
         try:
             if 'ph' in item['vkey']:
                 download(item['gif_url'], item['vkey'], 'webm')
-                # p = multiprocessing.Process(target=download, args=(item['gif_url'], item['vkey'], 'webm'))
-                # p.start()
         except Exception as err:
             logger.error(err)
 
 
 def detail_page(url):
     s = requests.Session()
-    resp = s.get(url, headers=headers, proxies=proxies)
+    resp = s.get(url, headers=headers, proxies=proxies, verify=False)
     html = etree.HTML(resp.content)
+
     title = ''.join(html.xpath('//h1//text()')).strip()
     logger.info(title)
 
-    js = html.xpath('//*[@id="player"]/script/text()')[0]
-    tem = re.findall('var\\s+\\w+\\s+=\\s+(.*);\\s+var player_mp4_seek', js)[-1]
-    con = json.loads(tem)
+    js_temp = html.xpath('//script/text()')
+    for j in js_temp:
+        if 'flashvars' in j:
+            js = ''.join(j.split('\n')[:-8])
+            videoUrl = exeJs(js)
+            logger.info(videoUrl)
+            download(videoUrl, title, 'mp4')
+            continue
 
-    for _dict in con['mediaDefinitions']:
-        if 'quality' in _dict.keys() and _dict.get('videoUrl'):
-            logger.info('%s %s' % (_dict.get('quality'), _dict.get('videoUrl')))
-            logger.info('start download...')
-            try:
-                download(_dict.get('videoUrl'), title, 'mp4')
-                break  # 如下载了较高分辨率的视频 就跳出循环
-            except Exception as err:
-                logger.error(err)
+
+def exeJs(js):
+    flashvars = re.findall('flashvars_\d+', js)[0]
+    res = js2py.eval_js(js + flashvars)
+    if res.quality_720p:
+        return res.quality_720p
+    elif res.quality_480p:
+        return res.quality_480p_
+    elif res.quality_240p:
+        return res.quality_240p
+    else:
+        logger.error('parse url error')
 
 
 def download(url, name, filetype):
@@ -88,24 +94,21 @@ def run(_arg=None):
             os.mkdir(path)
     if _arg == 'webm':
         # https://www.pornhub.com/categories
-        urls = ['https://www.pornhub.com/video?o=tr', 'https://www.pornhub.com/video?o=ht',
-                'https://www.pornhub.com/video?o=mv', 'https://www.pornhub.com/video']
+        urls = [
+            'https://www.pornhub.com/video?o=tr', 'https://www.pornhub.com/video?o=ht',
+            'https://www.pornhub.com/video?o=mv', 'https://www.pornhub.com/video'
+        ]
         for url in urls:
             list_page(url)
-            # p = multiprocessing.Process(target=list_page, args=(url, ))
-            # p.start()
     elif _arg == 'mp4':
         with open('download.txt', 'r') as file:
             keys = list(set(file.readlines()))
-        # jobs = []
         for key in keys:
             if not key.strip():
                 continue
             url = 'https://www.pornhub.com/view_video.php?viewkey=%s' % key.strip()
             logger.info('url: {}', url)
             detail_page(url)
-            # p = multiprocessing.Process(target=detail_page, args=(url, ))
-            # p.start()
     else:
         _str = """
 tips:
