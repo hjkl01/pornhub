caa087f11b8d723ff6a1f18f39acf774f5bfe0f7
ycm76229@gmail.com <ycm76229@gmail.com>
2020-05-16 00:01:38 +0800

    update parse download url function

--- crawler.py
+++ crawler.py
@@ -9,37 +9,40 @@ from lxml import etree
 from clint.textui import progress
 import fire
 from loguru import logger
-logger.add("logs/%s.log" % __file__.rstrip('.py'),
-           format="{time:MM-DD HH:mm:ss} {level} {message}")
+
+logger.add(
+    "logs/%s.log" % __file__.rstrip(".py"),
+    format="{time:MM-DD HH:mm:ss} {level} {message}",
+)
 
 headers = {
-    'User-Agent':
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+    "User-Agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
 }
 proxies = {}
 
 # 如果代理不稳定，不推荐使用
-# local showdowsocks service
+# local proxy service
 # proxies example:
-# proxies = {
-#     "http": "socks5://127.0.0.1:1080",
-#     "https": "socks5://127.0.0.1:1080",
-# }
+proxies = {
+    "http": "socks5://127.0.0.1:1080",
+    "https": "socks5://127.0.0.1:1080",
+}
 
 
 def list_page(url):
-    logger.info('crawling : %s' % url)
+    logger.info("crawling : %s" % url)
     resp = requests.get(url, headers=headers, proxies=proxies, verify=False)
     html = etree.HTML(resp.text)
     vkeys = html.xpath('//*[@class="phimage"]/div/a/@href')
     gif_keys = html.xpath('//*[@class="phimage"]/div/a/img/@data-mediabook')
     for i in range(len(vkeys)):
         item = {}
-        item['vkey'] = vkeys[i].split('=')[-1]
-        item['gif_url'] = gif_keys[i]
+        item["vkey"] = vkeys[i].split("=")[-1]
+        item["gif_url"] = gif_keys[i]
         try:
-            if 'ph' in item['vkey']:
-                download(item['gif_url'], item['vkey'], 'webm')
+            if "ph" in item["vkey"]:
+                download(item["gif_url"], item["vkey"], "webm")
         except Exception as err:
             logger.error(err)
 
@@ -49,22 +52,28 @@ def detail_page(url):
     resp = s.get(url, headers=headers, proxies=proxies, verify=False)
     html = etree.HTML(resp.content)
 
-    title = ''.join(html.xpath('//h1//text()')).strip()
+    title = "".join(html.xpath("//h1//text()")).strip()
     logger.info(title)
 
-    js_temp = html.xpath('//script/text()')
+    js_temp = html.xpath("//script/text()")
+    for js in js_temp:
+        with open('logs/test.js', 'a') as file:
+            file.write(js + '\n')
     for j in js_temp:
-        if 'flashvars' in j:
-            js = ''.join(j.split('\n')[:-8])
-            videoUrl = exeJs(js)
+        if "flashvars" in j:
+            videoUrl = exeJs(j)
             logger.info(videoUrl)
-            download(videoUrl, title, 'mp4')
+            download(videoUrl, title, "mp4")
             continue
 
 
 def exeJs(js):
     flashvars = re.findall('flashvars_\d+', js)[0]
+    js = ''.join(js.split('\n\t')[:-5]).strip()
+    # logger.debug(js)
+    logger.info(flashvars)
     res = js2py.eval_js(js + flashvars)
+    # logger.debug(res)
     if res.quality_720p:
         return res.quality_720p
     elif res.quality_480p:
@@ -72,20 +81,25 @@ def exeJs(js):
     elif res.quality_240p:
         return res.quality_240p
     else:
-        logger.error('parse url error')
+        logger.error("parse url error")
 
 
 def download(url, name, filetype):
-    filepath = '%s/%s.%s' % (filetype, name, filetype)
+    filepath = "%s/%s.%s" % (filetype, name, filetype)
     if os.path.exists(filepath):
-        logger.info('this file had been downloaded :: %s' % filepath)
+        logger.info("this file had been downloaded :: %s" % filepath)
         return
     else:
-        response = requests.get(url, headers=headers, proxies=proxies, stream=True)
+        response = requests.get(url,
+                                headers=headers,
+                                proxies=proxies,
+                                stream=True)
         with open(filepath, "wb") as file:
-            total_length = int(response.headers.get('content-length'))
-            for ch in progress.bar(response.iter_content(chunk_size=2391975),
-                                   expected_size=(total_length / 1024) + 1):
+            total_length = int(response.headers.get("content-length"))
+            for ch in progress.bar(
+                    response.iter_content(chunk_size=2391975),
+                    expected_size=(total_length / 1024) + 1,
+            ):
                 if ch:
                     file.write(ch)
 
@@ -98,30 +112,32 @@ def download(url, name, filetype):
         # with open(filepath, 'wb') as file:
         #     file.write(rep.content)
         # urllib.request.urlretrieve(url, '%s' % filepath)
-        logger.info('download success :: %s' % filepath)
+        logger.info("download success :: %s" % filepath)
 
 
 def run(_arg=None):
-    paths = ['webm', 'mp4']
+    paths = ["webm", "mp4"]
     for path in paths:
         if not os.path.exists(path):
             os.mkdir(path)
-    if _arg == 'webm':
+    if _arg == "webm":
         # https://www.pornhub.com/categories
         urls = [
-            'https://www.pornhub.com/video?o=tr', 'https://www.pornhub.com/video?o=ht',
-            'https://www.pornhub.com/video?o=mv', 'https://www.pornhub.com/video'
+            # 'https://www.pornhub.com/video?o=tr', 'https://www.pornhub.com/video?o=ht',
+            # 'https://www.pornhub.com/video?o=mv', 'https://www.pornhub.com/video'
+            "https://cn.pornhub.com/playlist/100503721"
         ]
         for url in urls:
             list_page(url)
-    elif _arg == 'mp4':
-        with open('download.txt', 'r') as file:
+    elif _arg == "mp4":
+        with open("download.txt", "r") as file:
             keys = list(set(file.readlines()))
         for key in keys:
             if not key.strip():
                 continue
-            url = 'https://www.pornhub.com/view_video.php?viewkey=%s' % key.strip()
-            logger.info('url: {}', url)
+            url = "https://www.pornhub.com/view_video.php?viewkey=%s" % key.strip(
+            )
+            logger.info("url: {}", url)
             detail_page(url)
     else:
         _str = """
@@ -134,8 +150,8 @@ tips:
         """
         logger.info(_str)
         return
-    logger.info('finish !')
+    logger.info("finish !")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fire.Fire(run)
