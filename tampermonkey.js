9f031df552258780eaa021529e9c1437da141dc7
ycm76229 <ycm76229@gmail.com>
2020-07-11 00:51:20 +0800

    add url after title

--- tampermonkey.js
+++ tampermonkey.js
@@ -8,40 +8,27 @@
 // @grant        none
 // ==/UserScript==
 
+
+
 (function() {
     'use strict';
 
     // Your code here...
 
 
-function copy(quality,url){
-const input = document.createElement('input');
-	document.body.appendChild(input);
- 	input.setAttribute('value', url);
-	input.select();
-	if (document.execCommand('copy')) {
-		document.execCommand('copy');
-		alert(quality + '复制成功');
-	}}
+    var qualites = [
+        "quality_1080p",
+        "quality_720p",
+        "quality_480p",
+        "quality_240p",
+    ];
 
-
-var qualites = [
-    "quality_1080p",
-    "quality_720p",
-    "quality_480p",
-    "quality_240p",
-];
-
-for (var b in window) {
-    if (b.includes("flashvars")) {
-        for (var i in qualites) {
-            if (window[b][qualites[i]]) {
-                copy(qualites[i], window[b][qualites[i]]);
-                break;
-            }
+    for (var i in qualites) {
+        if (window[qualites[i]]){
+            document.querySelector("h1").innerHTML += '<a href=' + window.quality_720p + '>' + qualites[i] + '</a>'
+            break
         }
-        break;
     }
-}
+
 
 })();
