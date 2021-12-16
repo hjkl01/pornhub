73fc2de66acf30854df03e7f9a7e27717a41119d
The handsome boy <ycm76229@gmail.com>
2021-12-16 16:23:35 +0800

    update : use youtube-dl

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-- go to https://github.com/ytdl-org/youtube-dl
 
+## 用法
 
-- [vultr邀请码](https://www.vultr.com/?ref=7378179)
-- [shadowsocks邀请码](https://portal.shadowsocks.nl/aff.php?aff=24252)
+```sh
+git clone https://github.com/lesssound/pornhub
+cd pornhub && pip install -r requirements.txt
+# 编辑 settings.toml, 配置proxy_url 和 喜欢的列表页面
+python crawler.py webm
+# 待程序运行完毕， 会在webm文件夹下download两页的webm缩略图，对应名称为详细页面的URL后缀
+python crawler.py mp4
+# 在MP4文件夹可看到下载好的MP4文件
+```
 
+### tips
+    - 可以在[vultr](https://www.vultr.com/?ref=7378179)上申请1小时的机器,下载完成后用scp download到本地
+    - 使用Trojan: [shadowsocks邀请码](https://portal.shadowsocks.nl/aff.php?aff=24252)
+    - 项目中用到了[youtube-dl](https://github.com/ytdl-org/youtube-dl)
--- crawler.py
+++ crawler.py
@@ -3,33 +3,27 @@
 import os
 import re
 
-import js2py
-import requests
-from lxml import etree
-from clint.textui import progress
 import fire
+import requests
+import youtube_dl
+from lxml import etree
 from loguru import logger
+from dynaconf import Dynaconf
 
-logger.add(
-    "logs/%s.log" % __file__.rstrip(".py"),
-    format="{time:MM-DD HH:mm:ss} {level} {message}",
-)
+settings = Dynaconf(settings_files=['settings.toml'])
+logger.add("logs/stdout.log", format="{time:MM-DD HH:mm:ss} {level} {message}")
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
 }
-proxies = {}
 
-# 如果代理不稳定，不推荐使用
-# local proxy service
-# proxies example:
 proxies = {
-    "http": "socks5://127.0.0.1:1080",
-    "https": "socks5://127.0.0.1:1080",
+    "http": settings.proxy_url,
+    "https": settings.proxy_url,
 }
 
 
-def list_page(url):
+def get_webm(url):
     logger.info("crawling : %s" % url)
     resp = requests.get(url, headers=headers, proxies=proxies, verify=False)
     html = etree.HTML(resp.text)
@@ -40,74 +34,32 @@ def list_page(url):
     for i in range(len(urls)):
         try:
             url = urls[i]
-            name = re.findall("=ph(\w+)", names[i])[-1]
-            logger.info(f"{url} {name}")
+            name = re.findall(r"=ph(\w+)", names[i])[-1]
+            #  logger.info(f"{url} {name}")
             download(url, name, "webm")
         except Exception as err:
             logger.error(err)
 
 
-def detail_page(url):
-    s = requests.Session()
-    resp = s.get(url, headers=headers, proxies=proxies, verify=False)
-    html = etree.HTML(resp.content)
-
-    title = "".join(html.xpath("//h1//text()")).strip()
-    logger.info(title)
-
-    js_temp = html.xpath("//script/text()")
-    for j in js_temp:
-        if "flashvars" in j:
-            videoUrl = exeJs(j)
-            # logger.debug(videoUrl)
-            download(videoUrl, title, "mp4")
-            continue
-
-
-def exeJs(js):
-    flashvars = re.findall("flashvars_\d+", js)[0]
-    js = "".join(js.split("\n\t")[:-5]).strip()
-    # logger.debug(js)
-    # logger.debug(flashvars)
-    res = js2py.eval_js(js + flashvars)
-    # logger.debug(res)
-    if res.quality_720p:
-        return res.quality_720p
-    elif res.quality_480p:
-        return res.quality_480p
-    elif res.quality_240p:
-        return res.quality_240p
-    else:
-        logger.error("parse url error")
-
-
 def download(url, name, filetype):
-    logger.info(f"{url} {name} {filetype}")
+    logger.debug(f"{url} {name} {filetype}")
     filepath = "%s/%s.%s" % (filetype, name, filetype)
     if os.path.exists(filepath):
         logger.info("this file had been downloaded :: %s" % filepath)
         return
     else:
-        response = requests.get(url, headers=headers, proxies=proxies, stream=True)
-        with open(filepath, "wb") as file:
-            total_length = int(response.headers.get("content-length"))
-            for ch in progress.bar(
-                response.iter_content(chunk_size=2391975),
-                expected_size=(total_length / 1024) + 1,
-            ):
-                if ch:
-                    file.write(ch)
+        rep = requests.get(url, headers=headers, proxies=proxies)
+        with open(filepath, 'wb') as file:
+            file.write(rep.content)
 
-        # from tqdm import tqdm
-        # with open(filepath, "wb") as handle:
-        #     for data in tqdm(response.iter_content()):
-        #         handle.write(data)
-
-        # rep = requests.get(url, headers=headers, proxies=proxies)
-        # with open(filepath, 'wb') as file:
-        #     file.write(rep.content)
         # urllib.request.urlretrieve(url, '%s' % filepath)
         logger.info("download success :: %s" % filepath)
+
+
+def get_mp4(urls):
+    ydl_opts = {'proxy': settings.proxy_url}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(urls)
 
 
 def run(_arg=None):
@@ -116,26 +68,16 @@ def run(_arg=None):
         if not os.path.exists(path):
             os.mkdir(path)
     if _arg == "webm":
-        # https://www.pornhub.com/categories
-        urls = [
-            # "https://www.pornhub.com/video?o=tr",
-            # "https://www.pornhub.com/video?o=ht",
-            # "https://www.pornhub.com/video?o=mv",
-            "https://www.pornhub.com/video",
-        ]
+        urls = settings.urls
         for url in urls:
-            list_page(url)
+            get_webm(url)
     elif _arg == "mp4":
         with open("download.txt", "r") as file:
             keys = list(set(file.readlines()))
-        logger.info(keys)
         keys += [d.strip(".webm") for d in os.listdir("webm/")]
-        for key in keys:
-            if not key.strip():
-                continue
-            url = "https://www.pornhub.com/view_video.php?viewkey=ph%s" % key.strip()
-            logger.info("url: {}", url)
-            detail_page(url)
+        logger.info(keys)
+        urls = ["https://www.pornhub.com/view_video.php?viewkey=ph%s" % key.strip() for key in keys]
+        get_mp4(urls)
     else:
         _str = """
 tips:
